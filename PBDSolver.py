83977034e8412634b5a11458b6e98605104ac3df
Dr Przemyslaw Korzeniowski <p.korzeniowski@krn-labs.com>
2026-03-17 13:49:21 +0100

    update to latest packages

--- PBDSolver.py
+++ PBDSolver.py
@@ -170,6 +170,7 @@ class PBDSolver(SolverXPBD):
                             state_in.body_q,
                             model.body_com,
                             model.joint_type,
+                            model.joint_enabled,
                             model.joint_parent,
                             model.joint_child,
                             model.joint_X_p,
